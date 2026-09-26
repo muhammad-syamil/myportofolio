d8e300f00bbcfe0a22296cff23a69df3978ecb85
Muhammad Syamil <muhammad.syamil51@ui.ac.id>
2026-09-26 22:43:54 +0700

    feat: add starring relation to experiences

--- main/models.py
+++ main/models.py
@@ -20,6 +20,12 @@ class Experience(models.Model):
     thumbnail = models.URLField(blank=True, null=True)
     started_at = models.DateTimeField(auto_now_add=True)
     ended_at = models.DateTimeField(blank=True, null=True)
+    starred_by = models.ManyToManyField(
+        User,
+        related_name="starred_experiences",
+        blank=True,
+    )
+
     def __str__(self):
         return self.title
     
